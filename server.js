dda47ad8e618d930736d2e07a1c5bc075818b5a9
nontawat kumbua <mac@nontawats-MacBook-Pro.local>
2026-04-05 12:29:28 +0700

    remove node_modules from git

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 const authRoutes = require('./src/routes/auth.routes')
 
 app.use(cors({                      
-  origin: 'http://localhost:5173'     
+  origin: 'https://login-practice-frontend-ebon.vercel.app/'     
 }))
-app.use(express.json()) // ล่ามแปลภาษา JSON ที่ประตู
+app.use(express.json()) 
 app.use('/api/auth' , authRoutes )
 
 const PORT = process.env.PORT || 3000
